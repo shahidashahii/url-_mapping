a5ee3b2724757d95d9572198ee7b1281bbeaac55
shahidashahii <shahidashahi2000@gmail.com>
2023-03-18 14:01:39 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def afrid(request):
+    return render(request,'afrid.html')
+
+def irfan(request):
+    return render(request,'irfan.html')
